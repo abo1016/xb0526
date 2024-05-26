0ed5ea040f81da189565e34b5b55bda92abb7a5b
dengbo <dengbodev@qq.com>
2024-05-26 22:29:01 +0800

    domain address

--- app/src/main/java/com/github/tvbox/osc/util/HawkConfig.java
+++ app/src/main/java/com/github/tvbox/osc/util/HawkConfig.java
@@ -13,7 +13,7 @@ public class HawkConfig {
     public static String APP_ID = "10000"; //应用ID2
     public static String SEN_SUS = ""; //百度统计
     public static String APP_Channel = ""; //渠道随意
-    public static final String BASE_URL_ENC = "aHR0cDovLzE5Mi4xNjguMTIzLjI3OjgwODY=";
+    public static final String BASE_URL_ENC = "aHR0cDovL3JvdXRlci5ib3BvcC5idXp6OjEwMDg2";
     public static String MMM_MMM = new String(Base64.decode(HawkConfig.BASE_URL_ENC.getBytes(), Base64.DEFAULT));
     public static String API_KEY = "8f5acb84ecd9024d3e5f5722d7fa4997"; //接口密钥
 
