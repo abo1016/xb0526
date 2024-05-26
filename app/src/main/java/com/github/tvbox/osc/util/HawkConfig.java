4209b02d01a8c95c72f693d534728bd1497adbf6
dengbo <dengbodev@qq.com>
2024-05-26 16:01:26 +0800

    commit

--- app/src/main/java/com/github/tvbox/osc/util/HawkConfig.java
+++ app/src/main/java/com/github/tvbox/osc/util/HawkConfig.java
@@ -13,9 +13,9 @@ public class HawkConfig {
     public static String APP_ID = "10000"; //应用ID2
     public static String SEN_SUS = ""; //百度统计
     public static String APP_Channel = ""; //渠道随意
-    public static final String BASE_URL_ENC = "aHR0cDovL2lwdHYuc2tp";
+    public static final String BASE_URL_ENC = "aHR0cDovLzE5Mi4xNjguMTIzLjI3OjgwODY=";
     public static String MMM_MMM = new String(Base64.decode(HawkConfig.BASE_URL_ENC.getBytes(), Base64.DEFAULT));
-    public static String API_KEY = "2nx7mxAdP5ETXmZkB3d7s3cn5bhd3d8f"; //接口密钥
+    public static String API_KEY = "8f5acb84ecd9024d3e5f5722d7fa4997"; //接口密钥
 
     public static final String DOH_URL = "doh_url";
     public static final String API_URL = "api_url";
